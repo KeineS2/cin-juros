ffd49b463abce13a91cacfcea7dbeefa08fbbef7
Keine <keine00@gmail.com>
2024-05-16 11:00:30 -0300

    Adiciona tipos de investimento

--- CIn_Juros/CIn_Juros/tipo_investimento.swift
+++ CIn_Juros/CIn_Juros/tipo_investimento.swift
@@ -10,9 +10,41 @@ import Foundation
 enum investimento: String, CaseIterable{
     case selic = "Tesouro Selic"
     case prefixado = "Tesouro Prefixado"
-    case posfixado = "Tesouro Pós-Fixado"
+    case ipca = "Tesouro IPCA+"
     case fundos = "Fundos Imobiliários"
-    case cdb = "C D B"
-    case lci = "LCI/LCA"
+    
+    func calcularJurosSelic (deValorInicial valorInicialSelic: Int, eAporteMensal aporteMensal: Int) -> Int{
+        
+        let resultado: Double
+        let r: Double
+        let n: Double
+        let valorCorrigido: Double
+        let valorCota: Double
+        let jurosCota: Double
+        let valorInicial: Double
+        let variacaoIpca: Double
+        let taxaJuros: Double
+        let t: Double
+        
+        switch self {
+        case .selic:
+            r = 0.05
+            n = 252
+        case .prefixado:
+            r = 0.08
+        case .ipca:
+            valorCorrigido = Double(valorInicial * (1 + variacaoIpca))
+            variacaoIpca = 0.05
+            taxaJuros = 0.06
+        case .fundos:
+            valorCota = 10
+            jurosCota = 0.1
+        }
+        
+        let resultado = valorInicialSelic * (1 + r / n) * n * t
+        
+        return resultado
+        
+    }
     
 }
